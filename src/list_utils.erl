5cb8b8e0c8df1f706ed79ca2a19b22ee76effab2
Mark LeBlanc <mark@embarkment.co>
2023-01-31 11:12:18 -0700

    Min Max implementation for lists in erlang.

--- src/list_utils.erl
+++ src/list_utils.erl
@@ -1,15 +1,30 @@
 -module(list_utils).
--export([min/1,max/1]).
+-export([min/1,max/1,min_max/1]).
 
+min_max([]) -> io:format("Cannot find min and max value of empty list.~n");
+min_max([H|T]) ->
+    Min = min([H|T]),
+    Max = max([H|T]),
+    { Min, Max }.
+
+min([]) -> io:format("Cannot find min value of empty list.~n");
 min([H|T]) ->
-    lmin(T, H).
+    lmin(H, T).
 
-max([H|_]) -> H.
+lmin(Min, [H|T]) ->
+    case Min < H of
+    true -> lmin(Min, T);
+    false -> lmin(H, T)
+    end;
+lmin(Min, []) -> Min.
 
-lmin([H|T], C) when H > C ->
-    lmin(T, C);
-lmin([H|T], C) when H < C ->
-    lmin(T, H);
-lmin([H|T], C) when H =:= C ->
-    lmin(T, C);
-lmin([], C) -> C.
+max([]) -> io:format("Cannot find max value of empty list.~n");
+max([H|T]) ->
+    lmax(H, T).
+
+lmax(Max, [H|T]) ->
+    case Max > H of
+    true -> lmax(Max, T);
+    false -> lmax(H, T)
+    end;
+lmax(Max, []) -> Max.

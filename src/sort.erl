36271b45eed1d5a93a61a57a081364eb7a706baa
Mark LeBlanc <mark@embarkment.co>
2023-02-03 12:07:06 -0700

    Bubble sort implementation.

--- src/sort.erl
+++ src/sort.erl
@@ -1,5 +1,13 @@
 -module(sort).
 -export([bubble_sort/1]).
 
-bubble_sort(_Arr) ->
-    ok.
+%% Slow O(N*N) but simple sort algorithm.
+bubble_sort([H|T]) ->
+    bubble_sort([H|T], [], false).
+
+bubble_sort([], Acc, true) -> lists:reverse(Acc);
+bubble_sort([], Acc, false) -> bubble_sort(lists:reverse(Acc), [], true);
+bubble_sort([P, S | T], Acc, _) when P > S -> bubble_sort([P | T], [S | Acc], false);
+bubble_sort([H|T], Acc, Halt) -> bubble_sort(T, [H | Acc], Halt).
+
+
